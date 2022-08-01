6a50aac11249b9718fe614af40c2a14fa60ec325
etozhebgdn <xeruvimov@gmail.com>
2022-08-01 19:27:20 +0400

    added comments

--- src/ru/surf/Index.java
+++ src/ru/surf/Index.java
@@ -9,18 +9,23 @@ import java.util.stream.Stream;
 
 
 public class Index {
+    //нет модификатора доступа, небезопасная публикация объекта
     TreeMap<String, List<Pointer>> invertedIndex;
 
+    //нет модификатора доступа, небезопасная публикация объекта
     ExecutorService pool;
 
     public Index(ExecutorService pool) {
         this.pool = pool;
+        //лучше указать this
         invertedIndex = new TreeMap<>();
     }
 
+    //по факту метод берёт из папки не только txt, название неподходящее
     public void indexAllTxtInPath(String pathToDir) throws IOException {
         Path of = Path.of(pathToDir);
 
+        //магическая константа
         BlockingQueue<Path> files = new ArrayBlockingQueue<>(2);
 
         try (Stream<Path> stream = Files.list(of)) {
@@ -32,10 +37,13 @@ public class Index {
         pool.submit(new IndexTask(files));
     }
 
+    //небезопасная публикация объекта
     public TreeMap<String, List<Pointer>> getInvertedIndex() {
         return invertedIndex;
     }
 
+    //название метода нарушает java notation
+    //небезопасная публикация объекта
     public List<Pointer> GetRelevantDocuments(String term) {
         return invertedIndex.get(term);
     }
@@ -67,17 +75,23 @@ public class Index {
             this.queue = queue;
         }
 
+        //метод берёт все слова в файле и считает сколько из них идентичны пути до этого файла
+        //добавляет данные об этом в invertedIndex
         @Override
         public void run() {
             try {
                 Path take = queue.take();
                 List<String> strings = Files.readAllLines(take);
 
+                //сложные лямбда выражения лучше выносить в отдельные методы
                 strings.stream().flatMap(str -> Stream.of(str.split(" "))).forEach(word -> invertedIndex.compute(word, (k, v) -> {
+                    //else можно опустить
                     if (v == null) return List.of(new Pointer(1, take.toString()));
                     else {
                         ArrayList<Pointer> pointers = new ArrayList<>();
 
+                        //сложные условия лучше выносить в отдельный метод
+                        //если условие true то выполнение можно заканчивать, т.к. следующий forEach ничего не сделает
                         if (v.stream().noneMatch(pointer -> pointer.filePath.equals(take.toString()))) {
                             pointers.add(new Pointer(1, take.toString()));
                         }
@@ -96,6 +110,7 @@ public class Index {
                 }));
 
             } catch (InterruptedException | IOException e) {
+                //надо бы залогировать
                 throw new RuntimeException();
             }
         }
